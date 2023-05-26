6656d6edcb6ea9614cd28301008c38940e8fcf55
scott-gianan <scott.gianan@gmail.com>
2023-05-26 11:20:51 +0800

    added 1 entry to card data array

---
+++ src/assets/card-data.jsx
@@ -0,0 +1,12 @@
+export default [
+  {
+    id: "wordle-clone",
+    src: "./card-images/wordle.png",
+    title: "Wordle Clone",
+    description:
+      "A web-based word game. You get six (6) chances to guess a five (5) letter word.",
+    alt: "wordle clone site preview",
+    repoUrl: "https://github.com/scott-gianan/wordle-clone/tree/master",
+    liveSiteUrl: "https://wordle-clone-by-scottg.netlify.app/",
+  },
+];
