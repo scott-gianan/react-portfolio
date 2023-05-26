c0709986a2bceb2d5c20639797c5641cf064b782
scott-gianan <scott.gianan@gmail.com>
2023-05-26 14:11:16 +0800

    populated 4 more items

--- src/assets/card-data.jsx
+++ src/assets/card-data.jsx
@@ -1,10 +1,50 @@
 export default [
   {
     id: "wordle-clone",
-    src: "./card-images/wordle.png",
+    src: "https://i.imgur.com/D6izdg5.png",
     title: "Wordle Clone",
     description:
       "A web-based word game. You get six (6) chances to guess a five (5) letter word.",
+    alt: "wordle clone site preview",
+    repoUrl: "https://github.com/scott-gianan/wordle-clone/tree/master",
+    liveSiteUrl: "https://wordle-clone-by-scottg.netlify.app/",
+  },
+  {
+    id: "tenzies",
+    src: "https://i.imgur.com/ITgatAZ.png",
+    title: "Tenzies",
+    description:
+      "Inspired by the dice game, TENZI® - a simple yet competitive game where the first player who rolls all 10 on the same number and yells “TENZI,” wins.",
+    alt: "tensizes site preview",
+    repoUrl: "https://github.com/scott-gianan/tenzies-game",
+    liveSiteUrl: "https://tenzies-by-scott.netlify.app/",
+  },
+  {
+    id: "meme-generator",
+    src: "https://i.imgur.com/oi5Fgjn.png",
+    title: "Meme Generator",
+    description:
+      "In Antarctica, there are 21 million penguins and in Malta there are 502,653 inhabitants. So if the penguins decide to invade Malta, each Maltese will have to fight 42 penguins.",
+    alt: "meme generator site preview",
+    repoUrl: "https://github.com/scott-gianan/meme-generator",
+    liveSiteUrl: "https://scotty-meme-generator.netlify.app/",
+  },
+  {
+    id: "lucky-21",
+    src: "https://i.imgur.com/XpZTofb.png",
+    title: "Lucky 21",
+    description:
+      "This browser game is inspired with the famous card games called Lucky 9 and Black Jack. Where objective of the game is to beat the dealer with a higher valued hand as close to 21 as possible. Tens and face cards are worth 10 points, aces are worth 1 point, and 2-9 are scored at face value.",
+    alt: "lucky 21 site preview",
+    repoUrl: "https://github.com/scott-gianan/lucky-21",
+    liveSiteUrl: "https://scott-gianan.github.io/lucky-21/",
+  },
+  {
+    id: "calculator",
+    src: "https://i.imgur.com/XRczZWr.png",
+    title: "Calculator",
+    description:
+      "Built a calculator to test my skills on basic HTML, CSS, and vanilla Javascript. This project is also included in the curriculum of The Odin Project - a free full-stack developer course.",
     alt: "wordle clone site preview",
     repoUrl: "https://github.com/scott-gianan/wordle-clone/tree/master",
     liveSiteUrl: "https://wordle-clone-by-scottg.netlify.app/",
