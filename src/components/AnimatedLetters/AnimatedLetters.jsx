a76194566526106f5377dca35c03744c08810c32
scott-gianan <scott.gianan@gmail.com>
2023-05-29 16:20:05 +0800

    reverted

--- src/components/AnimatedLetters/AnimatedLetters.jsx
+++ src/components/AnimatedLetters/AnimatedLetters.jsx
@@ -4,6 +4,8 @@ import { useState, useEffect } from "react";
 import "./AnimatedLetters.scss";
 
 function AnimatedLetters({ strArray, idx }) {
+  const [letterClass, setLetterClass] = useState("text-animate");
+
   useEffect(() => {
     const interval = setTimeout(() => {
       setLetterClass("text-animate-hover");
@@ -16,7 +18,7 @@ function AnimatedLetters({ strArray, idx }) {
     <span>
       {strArray.map((char, i) => {
         return (
-          <span key={char + i} className={`text-animate _${i + idx}`}>
+          <span key={char + i} className={`${letterClass} _${i + idx}`}>
             {char}
           </span>
         );
