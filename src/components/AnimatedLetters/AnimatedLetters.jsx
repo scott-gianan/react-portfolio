f98ebf221472e1ebb6e6f76043d9d40bbbfa23c4
scott-gianan <scott.gianan@gmail.com>
2023-05-29 16:06:39 +0800

    removed useState

--- src/components/AnimatedLetters/AnimatedLetters.jsx
+++ src/components/AnimatedLetters/AnimatedLetters.jsx
@@ -4,8 +4,6 @@ import { useState, useEffect } from "react";
 import "./AnimatedLetters.scss";
 
 function AnimatedLetters({ strArray, idx }) {
-  const [letterClass, setLetterClass] = useState("text-animate");
-
   useEffect(() => {
     const interval = setTimeout(() => {
       setLetterClass("text-animate-hover");
@@ -18,7 +16,7 @@ function AnimatedLetters({ strArray, idx }) {
     <span>
       {strArray.map((char, i) => {
         return (
-          <span key={char + i} className={`${letterClass} _${i + idx}`}>
+          <span key={char + i} className={`text-animate _${i + idx}`}>
             {char}
           </span>
         );
