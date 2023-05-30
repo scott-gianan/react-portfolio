ffa0bd38b34063b3a8e82ae072cc9b34dea75e72
scott-gianan <scott.gianan@gmail.com>
2023-05-30 15:35:07 +0800

    initial

---
+++ src/assets/skill-icons.jsx
@@ -0,0 +1,68 @@
+import {
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+  faNode,
+  faSass,
+  faBootstrap,
+  faPhp,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+export const skillIcons = [
+  {
+    id: "html5",
+    icon: faHtml5,
+    color: "#F06529",
+    iconText: "HTML5",
+  },
+  {
+    id: "css3",
+    icon: faCss3,
+    color: "#28A4D9",
+    iconText: "CSS3",
+  },
+  {
+    id: "javascript",
+    icon: faJsSquare,
+    color: "#EFD81D",
+    iconText: "JavaScript",
+  },
+  {
+    id: "bootstrap",
+    icon: faBootstrap,
+    color: "#563d7c",
+    iconText: "Bootstrap",
+  },
+  {
+    id: "react",
+    icon: faReact,
+    color: "#61dbfb",
+    iconText: "React JS",
+  },
+  {
+    id: "nodejs",
+    icon: faNode,
+    color: "#3c873a",
+    iconText: "Node JS",
+  },
+  {
+    id: "php",
+    icon: faPhp,
+    color: "#474A8A",
+    iconText: "PHP",
+  },
+  {
+    id: "git",
+    icon: faGitAlt,
+    color: "#474A8A",
+    iconText: "Git",
+  },
+  {
+    id: "github",
+    icon: faGithub,
+    color: "#474A8A",
+    iconText: "GitHub",
+  },
+];
