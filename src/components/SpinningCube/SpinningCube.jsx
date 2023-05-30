9ff3cc354fdeb1774971a25836f8d6aa7df8782c
scott-gianan <scott.gianan@gmail.com>
2023-05-30 14:59:10 +0800

    initial

---
+++ src/components/SpinningCube/SpinningCube.jsx
@@ -0,0 +1,16 @@
+import {
+  faNodeJs,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+  faNode,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const SpinningCube = () => {
+  return <FontAwesomeIcon icon={faReact} color="#5ED4F4" />;
+};
+
+export default SpinningCube;
