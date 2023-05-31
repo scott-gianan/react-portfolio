d9fc2b64482a353a977f4a645ea0fa0361889074
scott-gianan <scott.gianan@gmail.com>
2023-05-31 20:42:35 +0800

    final

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,36 @@
+import { Row, Col, Divider } from "antd";
+import "./Contact.scss";
+
 const Contact = () => {
-  return <>this is the contact page</>;
+  return (
+    <form className="form">
+      <Divider className="contact-divider">Leave a Message!</Divider>
+      <Row align="center">
+        <Col className="col-name" xs={18} md={14} lg={8}>
+          <label htmlFor="name">Name</label>
+          <input className="input" id="name" type="text" />
+        </Col>
+      </Row>
+      <Row align="center">
+        <Col className="col-email" xs={18} md={14} lg={8}>
+          <label htmlFor="email">Email</label>
+          <input className="input" id="email" type="email" required />
+        </Col>
+      </Row>
+      <Row align="center">
+        <Col className="col-message" xs={18} md={14} lg={8}>
+          <label htmlFor="message">Message</label>
+          <textarea className="input" id="message" required />
+        </Col>
+      </Row>
+      <Row align="center">
+        <Col xs={18} md={14} lg={8}>
+          <button className="btn-submit" type="submit">
+            Send a message!
+          </button>
+        </Col>
+      </Row>
+    </form>
+  );
 };
 export default Contact;
