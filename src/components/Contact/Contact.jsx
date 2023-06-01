dd3d83c82885f6074688fcb6c0dbea1a5ff7149e
scott-gianan <scott.gianan@gmail.com>
2023-06-01 11:13:58 +0800

    final

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,30 +1,64 @@
 import { Row, Col, Divider } from "antd";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import "./Contact.scss";
 
 const Contact = () => {
+  const refForm = useRef();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    emailjs
+      .sendForm(
+        "service_424sgvy",
+        "template_xznv8bg",
+        refForm.current,
+        "hHGqyFNAHXtKxqj8v"
+      )
+      .then(
+        () => {
+          window.alert("message sent!");
+          window.location.reload(false);
+        },
+        (e) => {
+          window.alert("something went wrong");
+          console.log(e);
+        }
+      );
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={handleSubmit} ref={refForm}>
       <Divider className="contact-divider">Leave a Message!</Divider>
-      <Row align="center">
-        <Col className="col-name" xs={18} md={14} lg={8}>
+      <Row align="center" gutter={8} className="form-row">
+        <Col className="col-name" xs={18} md={8} lg={6} xl={4}>
           <label htmlFor="name">Name</label>
-          <input className="input" id="name" type="text" />
+          <input className="input" id="name" type="text" name="name" />
         </Col>
-      </Row>
-      <Row align="center">
-        <Col className="col-email" xs={18} md={14} lg={8}>
+        <Col className="col-email" xs={18} md={8} lg={6} xl={4}>
           <label htmlFor="email">Email</label>
-          <input className="input" id="email" type="email" required />
+          <input
+            className="input"
+            id="email"
+            type="email"
+            name="email"
+            required
+          />
         </Col>
       </Row>
-      <Row align="center">
-        <Col className="col-message" xs={18} md={14} lg={8}>
+      <Row align="center" gutter={8} className="form-row">
+        <Col className="col-subject" xs={18} md={16} lg={12} xl={8}>
+          <label htmlFor="subject">Subject</label>
+          <input className="input" id="subject" name="subject" type="text" />
+        </Col>
+      </Row>
+      <Row align="center" gutter={8} className="form-row">
+        <Col className="col-message" xs={18} md={16} lg={12} xl={8}>
           <label htmlFor="message">Message</label>
-          <textarea className="input" id="message" required />
+          <textarea className="input" id="message" name="message" required />
         </Col>
       </Row>
-      <Row align="center">
-        <Col xs={18} md={14} lg={8}>
+      <Row align="center" gutter={8} className="form-row">
+        <Col xs={18} md={16} lg={12} xl={8}>
           <button className="btn-submit" type="submit">
             Send a message!
           </button>
